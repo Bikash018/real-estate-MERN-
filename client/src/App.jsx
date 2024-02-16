f23d1778786532b1d584c9b47b6eeb6407839cd4
Bikash Prasad <yaduvickey@gmail.com>
2024-02-16 21:41:21 +0530

    create signup ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter ,Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Signup from './pages/Signup'
+
+
+
 
 export default function App() {
   return (
     <BrowserRouter>
         <Routes> 
             <Route path='/' element = {<Home/>}/>
+            <Route path='/signup' element = {<Signup/>}/>
+            
           
         </Routes>
     </BrowserRouter>
