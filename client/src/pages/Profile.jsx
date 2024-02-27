b73766c98769a1959bfaa5df896320dca21dc52f
Bikash Prasad <yaduvickey@gmail.com>
2024-02-27 16:48:07 +0530

    profile image upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
+import {app} from "../firebase";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 const Profile = () => {
-  const {currentUser,loading } = useSelector((store)=>store.user)
+    const fileRef = useRef(null);
+    const {currentUser,loading } = useSelector((store)=>store.user)
+    const [filePerc,setFilePerc] = useState(0);
+    const [file, setFile] = useState(undefined);
+    const [FileUploadError, setFileUploadError] = useState(false)
+    const [FormData, setFormData] = useState({});
+
+    useEffect(() => {
+      if (file) {
+        handleFileUpload(file);
+      }
+    }, [file]);
+    
+    const handleFileUpload = (file) =>{
+    const storage = getStorage(app);
+    const filename = new Date().getTime() + file.name;
+    const storageRef =ref(storage,filename);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          setFilePerc(Math.round(progress));
+      
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+          setFileUploadError(true);
+        }, 
+        () => {
+      
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            setFormData({...FormData, avatar: downloadURL})
+          });
+        }
+      );
+
+  }
+  
   return (
  <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     <form  className='flex flex-col gap-4'>
-      <img
-       
-        src={currentUser.avatar}
-        alt='profile'
-        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
-      />
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          accept='image/*'
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={FormData.avatar || currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
       
       <input
         type='text'
